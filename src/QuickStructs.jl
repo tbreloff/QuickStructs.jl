d9163f642eb9594ea5aafd03125b49b3f681399a
Thomas Breloff <tom@breloff.com>
2015-06-05 10:17:04 -0400

    imported files

--- src/QuickStructs.jl
+++ src/QuickStructs.jl
@@ -1,5 +1,28 @@
 module QuickStructs
 
-# package code goes here
+
+export CircularBuffer,
+			 capacity,
+			 isfull,
+			 toarray,
+
+			 RollingWindow,
+
+			 DLLNode,
+			 DoublyLinkedList,
+			 head,
+			 tail,
+			 peek,
+
+			 SLLNode,
+			 SinglyLinkedList
+
+
+include("CircularBuffer.jl")
+include("RollingWindow.jl")
+
+include("SinglyLinkedList.jl")
+include("DoublyLinkedList.jl")
+
 
 end # module
